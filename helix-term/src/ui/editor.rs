00fb1e42b94906388b1fae74e7d0e3ccfc2746a5
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2025-05-02 18:14:17 +0100

    Add pinned indicator to bufferline.

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -647,7 +647,12 @@ impl EditorView {
                 bufferline_inactive
             };
 
-            let text = format!(" {}{} ", fname, if doc.is_modified() { "[+]" } else { "" });
+            let text = format!(
+                " {}{}{} ",
+                if doc.is_pinned { "^" } else { "" },
+                fname,
+                if doc.is_modified() { "[+]" } else { "" }
+            );
             let used_width = viewport.x.saturating_sub(x);
             let rem_width = surface.area.width.saturating_sub(used_width);
 
